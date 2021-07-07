090eaa040a7a4dcd700e9068f2fdb81241b8d852
Uncle2TN <86758625+Uncle2TN@users.noreply.github.com>
2021-07-07 16:35:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Thy à!',
+    introTitle: 'Nhung à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Trí đã yêu Thy từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng tui đã động lòng từ cái nhìn đầu tiên 😙',
     desc: 'Câu nói trên có đúng hay không ? ',
     btnYes: 'Chắc chắn đúng rồi 😳😳 !',
     btnNo: 'Không, sai hoàn toàn 😏😏',
-    question:'Liệu Thy có đáp lại hay hong ♥',
-    btnReply: 'Gửi cho Trí ♥',
+    question:'Liệu m có đáp lại hay hong ♥',
+    btnReply: 'Gửi cho Trí ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tui biết mà 🥰. Yêu Thy nhiều nhiều 😘😘',
-    messDesc: 'Học thật tốt nha để sau này Trí lo.',
+    mess: 'Tui biết mà 🥰.',
+    messDesc: 'Học thật tốt nha ♥',
     btnAccept: 'Okiiii lun <3',
     messLink: 'https://www.facebook.com/uncle202305' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
